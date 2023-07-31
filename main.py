40f6512161e0deb211ec920ee40988372551555d
jacdacatlover <jacdacatlover@gmail.com>
2023-07-31 20:47:40 +0800

    Day 32- Filtering forcast data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -8,6 +8,8 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                      ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+get_data(place, forecast_days, kind)
 
 def get_data(days):
     dates = ["2022-25-10","2022-26-10","2022-27-10"]
